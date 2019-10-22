58421e22c2cc19a1798c06cd925e0a295d879c50
Ezaz Hossain <46856988+Ezaz039@users.noreply.github.com>
2019-10-22 23:52:51 +0530

    Detect Cycle in Link List in C++.

---
+++ detect_cycle.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+//create a node that consist of data value and a next pointer;
+struct Node
+{
+	int data;
+	struct Node* next;
+};
+//create a new node;
+Node* getNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->next = NULL;
+	return temp;
+}
+//check whether the link list contain a cycle or not, if it has a cycle then the function will return
+//corresponding data value;
+void check_cycle(Node* head)
+{
+	Node *p, *q;
+	p = head;
+	q = head;
+	bool flag = false;
+	//take two pointer where one pointer will go one step forward and another will go two step forward
+	//untill any of them become null or both points to same note;
+	while(p and q and q->next)
+	{
+		p = p->next;
+		q = q->next->next;
+		if(p == q)		//this condition will execute if the link list have a cycle;
+		{
+			flag = true;
+			break;
+		}
+	}
+	if(!flag)	//if the link list don't have a cycle;
+	{
+		cout << "Link list does not have a cycle:: " << endl;
+		return;
+	}
+	p = head;
+	//traverse one pointer from starting node and another from joining node and each node go one step forward;
+	while(p != q)
+	{
+		p = p->next;
+		q = q->next;
+	}
+	cout << "Cycle present at number:: " << q->data << endl;
+}
+int main()
+{
+	Node* head = NULL;
+	Node* temp;
+	Node* r;
+	temp = head;
+	for(int i = 0; i < 10; i++)
+	{
+		if(i == 5)
+			r = temp;
+		if(!temp)
+		{
+			head = getNode(i*10);
+			temp = head;
+		}
+		else
+		{
+			temp->next = getNode(i*10);
+			temp = temp->next;
+		}
+	}
+	temp->next = r;
+	//check cycle in the link list;
+	check_cycle(head);
+	return 0;
+}
